2993ff8c7419c63ae92e897f42f8314689807a2b
Ryan Ma <ryanma@berkeley.edu>
2018-03-24 15:40:58 -0700

    Revert "base police functionality with hardcoded values"
    This reverts commit 291b6d07458020b17dc82c15557c80d5732c4c52.

---
+++ mobilePolice/app/screens/Contact/Component.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Modal, Text, TouchableHighlight, View} from 'react-native';
+
+
+class ContactComponent extends Component {
+
+}
+
+export default ContactComponent
