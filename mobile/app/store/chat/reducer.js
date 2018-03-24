397015d5fdc09c71b1fafaeacaee7abf1de47cec
Ryan Ma <ryanma21021@gmail.com>
2018-03-24 10:00:13 -0700

    Conversation context works FINALLY

--- mobile/app/store/chat/reducer.js
+++ mobile/app/store/chat/reducer.js
@@ -6,23 +6,29 @@ const initialState = {
   message: '',
   messages: {},
   loadMessagesError: null,
-  userId: Math.floor(Math.random() * 2147483647) + 1  
+  userId: Math.floor(Math.random() * 2147483647) + 1
 }
 
 const chat = (state = initialState, action) => {
   switch(action.type) {
     case types.CHAT_MESSAGE_LOADING:
       return { ...state, sending: true, sendingError: null }
+      break;
     case types.CHAT_MESSAGE_ERROR:
       return { ...state, sending: false, sendingError: action.error }
+      break;
     case types.CHAT_MESSAGE_SUCCESS:
       return { ...state, sending: false, sendingError: null, message: '' }
+      break;
     case types.CHAT_MESSAGE_UPDATE:
       return { ...state, sending: false, message: action.text, sendingError: null }
+      break;
     case types.CHAT_LOAD_MESSAGES_SUCCESS:
       return { ...state, messages: action.messages, loadMessagesError: null }
+      break;
     case types.CHAT_LOAD_MESSAGES_ERROR:
       return { ...state, messages: null, loadMessagesError: action.error }
+      break;
     default:
       return state
   }
