291b6d07458020b17dc82c15557c80d5732c4c52
Brian Chen <brianchen@Brians-MacBook-Pro.local>
2018-03-24 15:24:37 -0700

    base police functionality with hardcoded values

--- mobilePolice/app/screens/Contact/Component.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import {Modal, Text, TouchableHighlight, View} from 'react-native';
-
-
-class ContactComponent extends Component {
-
-}
-
-export default ContactComponent
--- mobilePolice/app/screens/Home/Home.js
+++ mobilePolice/app/screens/Home/Home.js
@@ -1,88 +1,53 @@
 import React, { Component } from 'react';
-import { 
-  View,
-  StyleSheet,
-  StatusBar,
-  Dimensions,
-  Text
-} from 'react-native';
 import {
-  Button,
-  Icon,
-  Input,
-} from 'react-native-elements';
-import ActionButton from 'react-native-action-button';
+  Platform,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import Row from '../FoldView/Row';
+import getIncidents from './Incidents';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    backgroundColor: '#4A637D',
+    flex: 1,
+    padding: 10,
+    paddingTop: 20,
+  },
+});
+
 
 export default class Home extends Component<Props> {
 
-  static navigationOptions = ({ navigation }) => {
-    const params = navigation.state.params || {};
-
-    return {
-      title: 'CitySense',
-      headerLeft: (
-        <Icon
-          name='menu'
-          color='#fff'
-          containerStyle={{
-            padding: 15
-          }}
-        />
-      ),
-      headerRight: (
-        <Icon
-          name='history'
-          color='#fff'
-          containerStyle={{
-            padding: 15
-          }}
-          onPress={() => params.navigate('ChatScreen')}
-          underlayColor='#2986d8'
-        />
-      )
-    }
-  };
-
-  componentWillMount() {
-    this.props.navigation.setParams({
-      navigate: this.props.navigation.navigate
-    });
-  };
-
   render () {
+    const incidents = getIncidents();
     return (
-      <View style={styles.homeContainer}>
-        <StatusBar/>
-        <View style={styles.list}>
-        </View>
-        <Input placeholder='What would you like to report?'/>
-        <Button title='Camera'/>
-        <Button title='Voice'/>
-        <Button title='Submit'
-                onPress={() => this.props.navigation.navigate('ChatScreen')}/>
-        <ActionButton
-          buttonColor='rgba(231,76,60,1)'
-          fixNativeFeedbackRadius={true}
-          onPress={() => this.props.navigation.navigate()}
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
         />
+        <ScrollView
+          style={styles.scrollView}
+        >
+          <Row zIndex={100}
+               data={incidents[0]}/>
+          <Row zIndex={90} 
+               data={incidents[1]}/>
+          <Row zIndex={80} 
+               data={incidents[2]}/>
+          <Row zIndex={70} 
+               data={incidents[3]}/>
+          <Row zIndex={60} 
+               data={incidents[4]}/>
+        </ScrollView>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  homeContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  },
-  header: {
-    height: 55
-  },
-  actionButtonIcon: {
-    fontSize: 20,
-    height: 22,
-    color: 'white'
-  }
-})
